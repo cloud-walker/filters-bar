b94252109511ce6c840e0003a28f2605f20be041
Luca Barone <baro.luc@gmail.com>
2026-07-14 20:57:52 +0200

    Update FiltersBar.tsx

--- src/ui/FiltersBar/FiltersBar.tsx
+++ src/ui/FiltersBar/FiltersBar.tsx
@@ -1,3 +1,22 @@
-export function FiltersBar(props: React.ComponentPropsWithRef<"div">) {
-  return <div {...props} />;
+export declare namespace FiltersBar {
+  interface Props extends React.ComponentPropsWithRef<"div"> {}
+}
+export function FiltersBar(props: FiltersBar.Props) {
+  return (
+    <div {...props}>
+      <select
+        value=""
+        onChange={(e) => {
+          console.log(e.target.value);
+        }}
+      >
+        <option value="" disabled hidden>
+          Add
+        </option>
+        <option value="banana">Banana</option>
+        <option value="ananas">Ananas</option>
+        <option value="apple">Apple</option>
+      </select>
+    </div>
+  );
 }
