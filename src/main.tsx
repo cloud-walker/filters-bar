95f5749df6770826d84c380a996fe1f3ad4ede91
Luca Barone <baro.luc@gmail.com>
2026-07-14 20:37:48 +0200

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+import { raise } from "./helpers/raise";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(
+  document.getElementById("root") ?? raise("#root element not found"),
+).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
